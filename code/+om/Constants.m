931ef23bbdf74da582d484c02f4c199d2826b4c2
Eivind Hennestad <ehennestad@gmail.com>
2023-03-21 19:00:32 +0100

    Fix path for schema directory

--- code/+om/Constants.m
+++ code/+om/Constants.m
@@ -3,9 +3,10 @@ classdef Constants < handle
     properties (Constant)
         GithubURL    = "https://github.com/HumanBrainProject/openMINDS"
         SchemaURL    = om.Constants.GithubURL + "/raw/documentation/openMINDS-v3.zip"
+        VocabURL     = om.Constants.GithubURL + "/raw/main/vocab"
         LogoLightURL = om.Constants.GithubURL + "/raw/main/img/light_openMINDS-logo.png";
         LogoDarkURL  = om.Constants.GithubURL + "/raw/main/img/dark_openMINDS-logo.png";
-        SchemaFolder = fullfile(om.Constants.getRootPath(), 'schema')
+        SchemaFolder = fullfile(om.Constants.getRootPath(), 'schemas')
     end
 
     methods (Static)
